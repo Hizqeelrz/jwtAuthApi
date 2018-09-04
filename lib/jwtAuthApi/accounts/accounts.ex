171251dfa8868d1776c5d4f6cc27d600570eca9a
hizqeelrz <hizqeel1214@gmail.com>
2018-09-05 01:57:01 +0500

    context authenticate functions added with sign_in action

--- lib/jwtAuthApi/accounts/accounts.ex
+++ lib/jwtAuthApi/accounts/accounts.ex
@@ -8,6 +8,9 @@ defmodule JwtAuthApi.Accounts do
 
   alias JwtAuthApi.Accounts.User
 
+  alias JwtAuthApi.Guardian
+  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+
   @doc """
   Returns the list of users.
 
@@ -101,4 +104,39 @@ defmodule JwtAuthApi.Accounts do
   def change_user(%User{} = user) do
     User.changeset(user, %{})
   end
+
+  # Context Authentication Fucntions
+
+  def token_sign_in(email,password) do
+    case email_password_auth(email, password) do
+      {:ok, user} ->
+        Guardian.encode_and_sign(user)
+      _ ->
+        {:error, :unauthorized}
+    end
+  end
+
+  defp email_password_auth(email, password) when is_binary(email) and is_binary(password) do
+    with {:ok, user} <- get_by_email(email),
+    do: verify_password(password, user)
+  end
+
+  defp get_by_email(email) when is_binary(email) do
+    case Repo.get_by(User, email: email) do
+      nil ->
+        dummy_checkpw()
+        {:error, "Login error."}
+      user ->
+       {:ok, user}
+    end
+  end
+
+  defp verify_password(password, %User{} = user) when is_binary(password) do
+    if checkpw(password, user.password_hash) do
+      {:ok, user}
+    else
+      {:error, :invalid_password}
+    end
+  end
+
 end
--- lib/jwtAuthApi_web/controllers/user_controller.ex
+++ lib/jwtAuthApi_web/controllers/user_controller.ex
@@ -8,6 +8,18 @@ defmodule JwtAuthApiWeb.UserController do
 
   action_fallback JwtAuthApiWeb.FallbackController
 
+  def sign_in(conn, %{"email" => email, "password" => password}) do
+    case Accounts.token_sign_in(email,password) do
+      {:ok, token, _claims} ->
+        conn
+        |> put_status(:created)
+        |> render("jwt.json", jwt: token)
+      _ ->
+        {:error, :unauthorized}
+    end
+  end
+
+
   def index(conn, _params) do
     users = Accounts.list_users()
     render(conn, "index.json", users: users)
