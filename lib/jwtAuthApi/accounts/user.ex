7e9284916f0beead22d7896c7ae6b84cd70d7765
hizqeelrz <hizqeel1214@gmail.com>
2018-09-05 01:02:50 +0500

    Account Context created with password hased

---
+++ lib/jwtAuthApi/accounts/user.ex
@@ -0,0 +1,42 @@
+defmodule JwtAuthApi.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  import Comeonin.Bcrypt, only: [hashpwsalt: 1]
+
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "users" do
+    field :email, :string
+    field :password_hash, :string
+
+    # virtual field added for password_hash
+
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :password, :password_confirmation])
+    |> validate_required([:email, :password, :password_confirmation])
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 8)
+    |> validate_confirmation(:password)
+    |> unique_constraint(:email)
+    |> save_password
+  end
+
+  defp save_password(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(changeset, :password_hash, hashpwsalt(password))
+      _ ->
+        changeset0
+    end
+  end
+end
