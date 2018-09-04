175ea23153d18f3e9dd591d7fd1384dbde4f4372
hizqeelrz <hizqeel1214@gmail.com>
2018-09-05 02:47:00 +0500

    show action updated

--- lib/jwtAuthApi_web/controllers/user_controller.ex
+++ lib/jwtAuthApi_web/controllers/user_controller.ex
@@ -34,9 +34,15 @@ defmodule JwtAuthApiWeb.UserController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    user = Accounts.get_user!(id)
-    render(conn, "show.json", user: user)
+  # def show(conn, %{"id" => id}) do
+  #   user = Accounts.get_user!(id)
+  #   render(conn, "show.json", user: user)
+  # end
+
+  def show(conn, _params) do
+    user = Guardian.Plug.current_resource(conn)
+    conn
+    |> render("user.json", user: user)
   end
 
   def update(conn, %{"id" => id, "user" => user_params}) do
